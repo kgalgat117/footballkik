0e5be8f035e11b945320aed9022d30ac75257646
karan <kgalgat117@gmail.com>
2017-12-15 00:35:10 +0530

    modules for local authentication

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express       = require('express');
 const bodyParser    = require('body-parser');
 const ejs           = require('ejs');
 const http          = require('http');
+const cookieParser  = require('cookie-parser');
+const validator     = require('express-validator');
+const session       = require('express-session');
+const MongoStore    = require('connect-mongo');
+const mongoose      = require('mongoose');
+const flash         = require('flash');
+
+
 const container     = require('./container');
 
 
-
-
 container.resolve(function(users){
+    
+    mongoose.promise = global.promise;
+    mongoose.connect('mongodb://localhost/footballkik', {useMongoClient: true});
+    
     const app = SetupExpress();
     
     function SetupExpress(){
@@ -25,12 +35,23 @@ container.resolve(function(users){
     }
     
     
-    
     function ConfigureExpress(app){
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+        
+        app.use(validator());
+        app.use(session({
+            secret: 'thisisasecretkey',
+            resave: true,
+            saveInitialized: true,
+            stroe: new MongoStore({
+                mongooseConnection: mongoose.connection
+            })
+        }));
+        app.use(flash());
     }
     
 });
